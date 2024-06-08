c1b15dea3dcb4d85dc7187dfe8e04f9fdb23a2cc
Eric <erictao6666@gmail.com>
2024-06-08 14:24:38 -0700

    test

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.onload = function(){
     let url;
 
+    // let data = {
+
+    // }
+
     document.getElementById("submit").onclick = function(){
         url = document.getElementById("url").value;
         document.getElementById("website").textContent = url
     }
+
+    // fetch(url, {
+    //     method: "POST",
+
+    // })
 }
