24d6e0e3d80485516fdc90f9c77ce040ca471d7c
Eric <erictao6666@gmail.com>
2024-06-09 13:19:04 -0700

    simulated the backend

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+var url;
+var money=0;
+var transaction_id = 34567867564
+
+var data = {
+    'csrf_token': '1Psy8rmSoK-VJGJ-rzSshYH8wyTUzqvwC-yg-g4KZp57YG-FkzoviwYnS4c_0MHtzAcR7InEwJ3jOqphWPPwDw47jgzO4CFgxGIH10AQs0z3zuuLBkvcbnZx3AjI6D6xzrAX-iwfAY3Hx0tz2YxBeoBzmndjwCYZnBshsdesGvIxSvcOuww=',
+    'agreeToSubscription': 'false'
+};
+
+function printResult(){
+    url = document.getElementById("url").value;
+    document.getElementById("website").textContent = url
+
+    money += 0.05
+    money = Math.round(money * 1e12) / 1e12
+    document.getElementById("money").textContent = "You just costed this website $"+money+"!"
+
+    transaction_id++
+    document.getElementById("output").textContent = "Transaction Declined: The cc payment type [Creditcard] and/or currency [USD] is not accepted REFID: "+ transaction_id
+
+    document.getElementById("data").textContent = "csrf_token" + data['csrf_token']
+}
+
 window.onload = function(){
-    let url;
-    let money=0;
+
     //https://www.dickies.ca/on/demandware.store/Sites-DickiesCA-Site/en_CA/CheckoutServices-PlaceOrder?ajax=true
 
     document.getElementById("submit").onclick = function(){
-        url = document.getElementById("url").value;
-        document.getElementById("website").textContent = url
-        money += 0.05
-        money = Math.round(money * 1e12) / 1e12
-        document.getElementById("money").textContent = "You just costed this scammer $"+money+"!"
+        setTimeout(printResult, 2000)
+        document.getElementById("website").textContent = "processing, please wait..."
     }
-
-    let data = {
-        'csrf_token': '1Psy8rmSoK-VJGJ-rzSshYH8wyTUzqvwC-yg-g4KZp57YG-FkzoviwYnS4c_0MHtzAcR7InEwJ3jOqphWPPwDw47jgzO4CFgxGIH10AQs0z3zuuLBkvcbnZx3AjI6D6xzrAX-iwfAY3Hx0tz2YxBeoBzmndjwCYZnBshsdesGvIxSvcOuww=',
-        'agreeToSubscription': 'false'
-    };
 
     fetch(url, {
         method: "POST",
